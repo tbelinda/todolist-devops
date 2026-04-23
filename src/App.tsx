f1eb86047c6efaf7e0fc1a33e1dd8575090b7d7f
tbelinda <belinda95610@gmail.com>
2026-04-23 11:20:35 +0200

    fix App.tsx syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-
-export default App;
-import "./App.css";
-import { ThemeProvider } from "@mui/material";
-import { theme } from "./utils/styles/theme";
-import Dashboard from "./MainView/5-panels/Dashboard";
+import React from "react";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Dashboard />
-    </ThemeProvider>
+    <div>
+      Todo List App
+    </div>
   );
-
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-    console.log("");
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-
 }
-
