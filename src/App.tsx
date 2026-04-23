e8ec939eb9402f72f6665869bc3ebd474e352bd4
tbelinda <belinda95610@gmail.com>
2026-04-23 11:39:14 +0200

    restore App.tsx features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
+import "./App.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./utils/styles/theme";
+import Dashboard from "./MainView/5-panels/Dashboard";
 import React from "react";
 
 function App() {
   return (
-    <div>
-      Todo List App
-    </div>
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
-}
 
-export default App;
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    console.log("");
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+    export default App;
+}
