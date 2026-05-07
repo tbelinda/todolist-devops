f455b1b5a1980c624151743954250ee5fb625e55
tbelinda <belinda95610@gmail.com>
2026-05-07 09:43:38 +0200

    restore clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
+import React from "react";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./utils/styles/theme";
 import Dashboard from "./MainView/5-panels/Dashboard";
-import React from "react";
+import "./App.css";
 
 function App() {
   return (
@@ -10,30 +10,6 @@ function App() {
       <Dashboard />
     </ThemeProvider>
   );
-
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-    console.log("");
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-    export default App;
 }
+
+export default App;
